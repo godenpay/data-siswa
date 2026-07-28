fee8338bda90cc03191e8b7f273e080acd246f56
godenpay <godenpay@gmail.com>
2026-07-29 00:46:03 +0700

    init: data siswa

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyj7nX_RpOSEsKM3E_jZzwp5bkTIWBK4bM1NlhelYr4QVwPTPlOnbXGh3vIkDG1eP7LcQ/exec',
   APP_NAME: 'Data Siswa',
 };
